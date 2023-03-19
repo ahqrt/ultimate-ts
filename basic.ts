4a01f5facad6eb6e9b55971a4e69deb17582bfd7
ahqrt <1217223113@qq.com>
2023-03-19 15:47:53 +0800

    feat: add union and intersection

--- basic.ts
+++ basic.ts
@@ -126,3 +126,25 @@ console.log(obj.name);
 function fail(): never {
   throw new Error("123");
 }
+
+interface Colorful {
+  color: string;
+}
+interface Circle {
+  radius: number;
+}
+
+type ColorfulCircle = Colorful & Circle;
+
+type ColorfulUnion = Colorful | Circle;
+
+type InterSection = string & number;
+// union: you must chose single of the all types
+// intersection : you can use all of the all types
+
+function draw(circle: ColorfulCircle) {
+  console.log(`Color was ${circle.color}`);
+  console.log(`Radius was ${circle.radius}`);
+}
+
+draw({ color: "blue", radius: 42 });
