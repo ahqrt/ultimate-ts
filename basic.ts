1129b9445b23a5269c509c6d7edd1e7341f2e3c9
ahqrt <1217223113@qq.com>
2023-03-19 15:32:30 +0800

    feat: add generic

--- basic.ts
+++ basic.ts
@@ -49,3 +49,80 @@ const item = filter(["1", "2", "3"], (n) => {
 });
 
 console.log(item);
+
+function combine<T, U>(arr1: T[], arr2: U[]) {
+  // return arr1.concat(arr2);
+  return [...arr1, ...arr2];
+}
+
+const arr = combine([1, 2, 3], ["hello"]);
+console.log(arr);
+
+function makeDate(timestamp: number): Date;
+function makeDate(m: number, d: number, y: number): Date;
+
+function makeDate(mOrTimestamp: any, d?: any, y?: any): any {
+  if (d !== undefined && y !== undefined) {
+    return new Date(y, mOrTimestamp, d);
+  } else {
+    return new Date(mOrTimestamp);
+  }
+}
+
+function fn(x: string): string;
+// Return type isn't right
+function fn(x: number): boolean;
+// This overload signature is not compatible with its implementation signature.
+
+function fn(x: string | number): string | boolean {
+  if (typeof x === "string") {
+    return "opps";
+  } else {
+    return true;
+  }
+}
+
+fn("hello");
+fn(1);
+
+function len(s: string): number;
+function len<T>(arr: T[]): number;
+function len<T>(arr: T[] | string): number;
+function len(x: any) {
+  return x.length;
+}
+
+len(""); // OK
+len([0]); // OK
+// However, we can’t invoke it with a value that might be a string or an array
+let a = Math.random() > 0.5 ? "hello" : [0];
+
+len(a);
+
+// function safeParse(s: string): unknown {
+//   return JSON.parse(s);
+// }
+
+function safeParse<T>(s: string): T {
+  return JSON.parse(s);
+}
+
+interface RandomString {
+  name: string;
+}
+
+// Need to be careful with 'obj'!
+const someRandomString = { name: 12 };
+
+const obj = safeParse<RandomString>(JSON.stringify(someRandomString));
+console.log(obj.name);
+
+// void  never
+
+// function fail() {
+//   console.log(123)
+// }
+
+function fail(): never {
+  throw new Error("123");
+}
